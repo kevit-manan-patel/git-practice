3f181c89df3d552e533fb76f43c5695a48a063c9
Manan <manan.patel@kevit.io>
2026-09-03 12:53:02 +0530

    feature/login contains the login feature , feature/login contains the updated app.js

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # git-practice# Git Practice Project 
  
 This project is created for Git practice. 
+## Login Feature 
+ 
+Users can log in using their email and password. 
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Git practice project started"); 
+ 
+function login(username, password) { 
+  if (username && password) { 
+    return "Login successful"; 
+  } 
+ 
+  return "Username and password are required"; 
+} 
+ 
+console.log(login("intern", "git123")); 
